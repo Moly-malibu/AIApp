d6815f18e9ad77821ad8a83790305ccc46f3d3fb
Monica Bustamante <58006376+Moly-malibu@users.noreply.github.com>
2022-10-21 22:43:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -247,7 +247,7 @@ def IndustryAVG():
     ## Closing Price
 
     """)
-    source = source[df.symbol.isin(symbols)]
+    source = source[df.tickerSymbol.isin(symbols)]
     chart = chart.get_chart(source)
     st.altair_chart(chart, use_container_width=True)
     st.line_chart(tickerDf.Volume)
