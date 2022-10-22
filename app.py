fba3f08a0999fde39bb04a26128858c69b551090
Monica Bustamante <58006376+Moly-malibu@users.noreply.github.com>
2022-10-21 21:37:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ def IndustryAVG():
     page_bg_img = '''
     <style>
     .stApp {
-    background-image: url("https://img.freepik.com/free-photo/3d-geometric-abstract-cuboid-wallpaper-background_1048-9891.jpg?size=626&ext=jpg&ga=GA1.2.635976572.1603931911");
+    background-image: url("https://images.pexels.com/photos/1193743/pexels-photo-1193743.jpeg?cs=srgb&dl=pexels-dan-cristian-p%C4%83dure%C8%9B-1193743.jpg&fm=jpg&_gl=1*lhloxo*_ga*MTI1MDQwMzMyOS4xNjU5ODc1MzA2*_ga_8JE65Q40S6*MTY2NjQxMzM3OS4xMC4xLjE2NjY0MTM0MDAuMC4wLjA.");
     background-size: cover;
     }
     </style>
@@ -236,12 +236,12 @@ def IndustryAVG():
     st.markdown(page_bg_img, unsafe_allow_html=True)
     symbols = 'https://raw.githubusercontent.com/Moly-malibu/AIApp/main/industAVG.csv'
     df = pd.read_csv(symbols)
-    st.markdown("<h1 style='text-align: center; color: #002967;'>Stock Price </h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: #002967;'>Main Standards of US Stock Market</h1>", unsafe_allow_html=True)
     start = st.sidebar.date_input("Enter Date Begin Analysis: ") 
-    tickerSymbol = st.sidebar.selectbox('Stocks Close and Volume price by Company', (df))
+    tickerSymbol = st.sidebar.selectbox('Stocks Close and Volume price by Industry', (df))
     tickerData = yf.Ticker(tickerSymbol)
     tickerDf = tickerData.history(period='id', start=start, end=None)
-    st.write("""# Analysis of Data""")
+    st.write("""# Stock Market Behavior""")
     st.write("""
     ## Closing Price
 
@@ -252,12 +252,8 @@ def IndustryAVG():
     """)
     st.line_chart(tickerDf.Volume)
     st.markdown("<h1 style='text-align: center; color: #002967;'>Stock Price Compared</h1>", unsafe_allow_html=True)
-    st.write("""
-    **Business** and **Techology** are two fills that have changed the world, both occupy the main ratings in finance, being one of the most highly valued in the stock market leading their owners to be billionaires, in this simple application we can analyze the stock movement and prediction of future price of stock used algoriths and Machile Learning.
-    Show are the Stock **Closing Price** and ** Volume** of Stocks by year!
-    """)
     st.markdown('Help to take algoritmc decision about stocks')
-    company = tickerSymbol1 = st.sidebar.multiselect("Select Companies Stock be compared", (df))
+    company = tickerSymbol1 = st.sidebar.multiselect("Select Industry Stock be compared", (df))
     if company:
         st.subheader("""**Compared Status**""")
         button_clicked = st.sidebar.button("GO")
@@ -266,7 +262,7 @@ def IndustryAVG():
         analysis['Adj Close'].plot()
         plt.xlabel("Date")
         plt.ylabel("Adjusted")
-        plt.title("Company Stock")
+        plt.title("Industry")
         st.set_option('deprecation.showPyplotGlobalUse', False)
         st.pyplot()
 
